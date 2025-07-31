dc2b965b06029fa22bf26647166f8beb841d1f33
congdaika <maibubu2001@gmail.com>
2025-07-31 03:59:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Toggle the menu on click
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+}
